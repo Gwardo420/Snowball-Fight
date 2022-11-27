e7998eaef7d6ac600f3d47e46b27a4d80bf51cc7
Gwardo420's Projects <torreykimbrough@gmail.com>
2022-11-26 20:39:27 -0500

    Added the map

--- public/index.js
+++ public/index.js
@@ -1,5 +1,54 @@
+const mapImage = new Image();
+mapImage.src = '/snow.png';
+
+const canvasElement = document.getElementById('canvas');
+canvasElement.width = window.innerWidth;
+canvasElement.height = window.innerHeight;
+const canvas = canvasElement.getContext('2d');
+
 const socket = io('ws://localhost:3000');
 
+let map = [[]];
+
+const TILE_SIZE = 32;
+
 socket.on('connect', function(socket) {
-  console.log(socket)
-})
+  console.log(socket);
+});
+
+socket.on('map', (loadedMap) => {
+  map = loadedMap;
+});
+
+
+function loop() {
+  canvas.clearRect(0, 0, canvas.width, canvas.height)
+  
+  const TILES_IN_ROW = 16;
+
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[0].length; col++) {
+      const { id } = map[row][col];
+      const imageRow = parseInt(id / TILES_IN_ROW);
+      const imageCol = id % TILES_IN_ROW;
+
+      // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+      
+      canvas.drawImage(
+        mapImage, 
+        imageCol * TILE_SIZE,
+        imageRow * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+        col * TILE_SIZE, 
+        row * TILE_SIZE, 
+        TILE_SIZE, 
+        TILE_SIZE
+      )
+    }
+  }
+
+  window.requestAnimationFrame(loop);
+}
+
+window.requestAnimationFrame(loop);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const { createServer } = require("http");
+const { resolve } = require('path');
 const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
 
-const io = new Server(httpServer)
+const io = new Server(httpServer);
+const loadMap = require("./mapLoader");
 
-io.on('connect', (socket) => {
-  console.log("User connected to ", socket.id)
-})
+async function main() {
+  const map2D = await loadMap();
 
-app.use(express.static("public"));
-
-httpServer.listen(3000);
+  io.on('connect', (socket) => {
+    console.log("User connected to ", socket.id);
+  
+    socket.emit("map", map2D);
+  });
+  
+  app.use(express.static("public"));
+  
+  httpServer.listen(3000);
+}
+main()
