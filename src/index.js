9f88d12b050c1461417c5ee7323a195236d9d151
Gwardo420's Projects <torreykimbrough@gmail.com>
2022-11-27 22:12:27 -0500

    port

--- src/index.js
+++ src/index.js
@@ -155,7 +155,7 @@ async function main() {
   
   app.use(express.static("public"));
   
-  httpServer.listen(3000);
+  httpServer.listen(PORT);
 
   let lastUpdate = Date.now();
   setInterval(() => {
