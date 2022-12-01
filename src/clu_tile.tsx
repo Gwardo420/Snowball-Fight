16607723b2122b88eb8fd4439da3386b6bfe72b4
Gwardo420's Projects <torreykimbrough@gmail.com>
2022-12-01 11:52:06 -0500

    Map update

--- src/clu_tile.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="clu_tile" tilewidth="16" tileheight="16" tilecount="225" columns="15" fillmode="preserve-aspect-fit">
- <transformations hflip="1" vflip="0" rotate="0" preferuntransformed="0"/>
- <image source="../../Downloads/pxArt.png" width="250" height="250"/>
-</tileset>
